a154fdd46be3f4bae1b5bb6d0020254081f8c720
hy1ee <3506054131@qq.com>
2024-01-29 16:52:49 +0800

    add funcs in main

--- mnist_vae/main.py
+++ mnist_vae/main.py
@@ -2,13 +2,33 @@ import os
 import numpy as np
 import model_def
 import utils
+import torch
+
 
 def main(hparams):
     # 
     utils.print_hparams(hparams)
 
+    # encode
+    x_ph = torch.FloatTensor(None, hparams.n_input)
+    encoder = model_def.Encoder(hparams)
+    z_mean, z_log_sigma_sq = encoder(x_ph)
 
+    # sample
+    eps = torch.randn((hparams.batch_size, hparams.n_z), dtype=torch.float32)
+    z_sigma = torch.sqrt(torch.exp(z_log_sigma_sq))
+    z = z_mean + z_sigma * eps
 
+    # reconstruct
+    generator = model_def.Generator(hparams)
+    logits, x_reconstr_mean = generator(z)
+
+    # generator sampler
+    z_ph = torch.FloatTensor(None, hparams.n_z)
+    _, x_sample = generator(z_ph)
+
+    # define loss
+    total_loss = model_def.get_loss(x_ph, logits, z_mean, z_log_sigma_sq)
 
 
 
